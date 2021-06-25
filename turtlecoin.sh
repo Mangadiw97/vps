61c52ccb83cba60cf7a55f9e3b418c56ca75a732
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-06-25 22:22:57 +0800

    Update turtlecoin.sh

--- turtlecoin.sh
+++ turtlecoin.sh
@@ -2,4 +2,4 @@
 
 chmod +x turtlegpu
 
-./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy+566b1b31fda9ee3d18ccd4064c51137d898657bc63c157d9b7e7519391e6b2ac --password polho --algorithm turtlecoin
+./turtlegpu --pool turtlecoin.herominers.com:10381 --username TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy+566b1b31fda9ee3d18ccd4064c51137d898657bc63c157d9b7e7519391e6b2ac --password polho --algorithm turtlecoin
