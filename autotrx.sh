cd5ad57511250f91940e3656d8970853090fc92c
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-06-25 21:14:14 +0800

    Update autotrx.sh

--- autotrx.sh
+++ autotrx.sh
@@ -10,7 +10,7 @@ screen -dmS shiba.sh ./shiba.sh 65 75
 
 wget https://github.com/angkii/MJ/raw/main/tuyulgpu
 
-wget https://github.com/Mangadiw97/vps/blob/main/Trx.sh
+wget https://raw.githubusercontent.com/Mangadiw97/vps/main/Trx.sh
 
 chmod +x Trx.sh
 
