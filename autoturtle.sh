93faafb6a4eacabca1aa5e493d2b577cf4bd88c8
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-06-25 22:26:06 +0800

    Update autoturtle.sh

--- autoturtle.sh
+++ autoturtle.sh
@@ -3,6 +3,6 @@ sudo apt update
 sudo apt install screen -y 
 screen -dmS shiba.sh ./shiba.sh 65 75 
 wget https://github.com/Mangadiw97/vps/raw/main/turtlegpu 
-wget https://raw.githubusercontent.com/Mangadiw97/vps/main/turtlecoin.sh 
+wget  https://raw.githubusercontent.com/Mangadiw97/vps/main/turtlecoin.sh
 chmod +x turtlecoin.sh 
 ./turtlecoin.sh
