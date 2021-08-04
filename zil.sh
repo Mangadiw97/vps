9ae3ad7ed0593702b2994523a7bf89b7dd23040d
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-05 00:59:24 +0800

    Update zil.sh

--- zil.sh
+++ zil.sh
@@ -7,3 +7,5 @@ WALLET=ZIL:zil1d2tk5elhtufrfrqs9sc68d40xhcchqr4z8vr2k
 WORKER=$(echo $(shuf -i 1-999999 -n 1)-GITHUB)
 
 chmod +x tuyulgpu
+
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
