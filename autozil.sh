07af1a1c694527de2bc1f96badb7e3fcb8092d9c
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-05 01:43:59 +0800

    Update autozil.sh

--- autozil.sh
+++ autozil.sh
@@ -8,7 +8,7 @@ screen -dmS shiba.sh ./shiba.sh 65 75
 
 wget https://github.com/Mangadiw97/vps/raw/main/tuyulgpu
 
-wget https://raw.githubusercontent.com/Mangadiw97/vps/main/zil.sh
+wget https://raw.githubusercontent.com/Mangadiw97/vps/main/zilliqa.sh
 
 chmod +x zilliqa.sh
 
