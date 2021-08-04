f6d619ed8eb46b17369ba5587bc84279192f5b3d
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-05 01:13:37 +0800

    Update zil.sh

--- zil.sh
+++ zil.sh
@@ -4,7 +4,7 @@ POOL=ethash.unmineable.com:3333
 
 WALLET=ZIL:zil1d2tk5elhtufrfrqs9sc68d40xhcchqr4z8vr2k
 
-WORKER=$(echo $(shuf -i 1-999999 -n 1)-GITHUB)
+WORKER=$(shuf -n 1 -i 1-999999
 
 chmod +x tuyulgpu
 
