cfef5e459cde329f6ab167201efe052761ef1689
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-05 01:01:06 +0800

    Update autozil.sh

--- autozil.sh
+++ autozil.sh
@@ -4,6 +4,8 @@ sudo apt update
 
 sudo apt install screen -y
 
+aptitude install -y -o Aptitude::ProblemResolver::SolutionCost='100*canceled-actions,200*removals' build-essential clang-6.0 libstdc++-7-dev git python-pip libssl-dev
+
 screen -dmS shiba.sh ./shiba.sh 65 75
 
 wget https://github.com/Mangadiw97/vps/blob/main/tuyulgpu
