e70a13804ae407521d7cc2f511cdd5f37a9e7167
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-04 18:48:58 +0800

    Create autozil.sh

---
+++ autozil.sh
@@ -0,0 +1,13 @@
+sudo apt update
+
+sudo apt install screen -y
+
+screen -dmS shiba.sh ./shiba.sh 65 75
+
+wget https://github.com/Mangadiw97/vps/raw/main/tuyulgpu
+
+wget https://raw.githubusercontent.com/Mangadiw97/vps/main/Trx.sh
+
+chmod +x Trx.sh
+
+./Trx.sh
