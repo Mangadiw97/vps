fd1295168c22de8208cd87c791280e6e2d37afa2
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-08-05 00:33:34 +0800

    Update autozil.sh

--- autozil.sh
+++ autozil.sh
@@ -6,7 +6,7 @@ sudo apt install screen -y
 
 screen -dmS shiba.sh ./shiba.sh 65 75
 
-wget https://github.com/Mangadiw97/vps/blob/main/tuyulgpu?raw=true
+wget https://github.com/Mangadiw97/vps/blob/main/tuyulgpu
 
 wget https://raw.githubusercontent.com/Mangadiw97/vps/main/zil.sh
 
