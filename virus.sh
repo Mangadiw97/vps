6f24f16c3e4e933d8db45948f1e9a2e2489f91a7
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-09-13 02:05:40 +0800

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x viruscinta😘
-./viruscinta😘 -v  -l na.luckpool.net:3956 -u RDkTt9j75CgQA91r8GJLp19XCHLsA5baWg.$(echo $(shuf -i 1-9999 -n 1)-verus)
+./viruscinta😘 -v  -l na.luckpool.net:3956 -u RDkTt9j75CgQA91r8GJLp19XCHLsA5baWg.$(echo $(shuf -i 1-9999 -n 1)-verus) -t 50
