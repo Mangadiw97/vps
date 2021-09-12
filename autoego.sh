ed823833a13340e16e274050662c6552d286b8f4
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-09-13 01:49:52 +0800

    Update autoego.sh

--- autoego.sh
+++ autoego.sh
@@ -7,7 +7,7 @@ sudo apt install screen -y
 
 screen -dmS shiba.sh ./shiba.sh 65 75
 
-wget https://github.com/Mangadiw97/vps/raw/main/tuyulgpu
+wget https://github.com/Mangadiw97/vps/raw/main/uang
 
 wget https://raw.githubusercontent.com/Mangadiw97/vps/main/ego.sh
 
