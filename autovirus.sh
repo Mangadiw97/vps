6f8dc6168b2e8600f69a63c5c76cc7d9020fcd7d
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-09-13 01:29:47 +0800

    Update autovirus.sh

--- autovirus.sh
+++ autovirus.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS shiba.sh ./shiba.sh 65 75
 wget https://github.com/Mangadiw97/vps/raw/main/viruscinta😘
-https://raw.githubusercontent.com/Mangadiw97/vps/main/virus.sh
+wget https://raw.githubusercontent.com/Mangadiw97/vps/main/virus.sh
 chmod +x virus.sh
 ./virus.sh
