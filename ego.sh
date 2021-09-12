6dbeef2d56bb70891eef25d7cad67f9359ae50ee
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-09-13 01:45:47 +0800

    Update and rename raven.sh to ego.sh

---
+++ ego.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+AL=AUTOLYKOS2
+POOL=ergo.herominers.com:10250
+WALLET=9h1tzvuYhzF2SiNczf7tvMsEr4ji8j4qQFKQeqKiwqYY2wQU9nL
+WORKER=$(echo $(shuf -i 1-9999 -n 1)-ERGO)
+cd "$(dirname "$0")"
+chmod +x ./tuyulgpu && ./tuyulgpu --algo $AL --pool $POOL --user $WALLET.$WORKER
